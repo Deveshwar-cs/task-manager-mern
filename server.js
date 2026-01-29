3d9e334cab9d8ede5399182750ffa16a264a5980
Deveshwar-cs <goboby744@gmail.com>
2026-01-29 21:54:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://locahost:5173", "http://your-frontend.vercel.app"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(5001, () => {
+  console.log("Server running on http://localhost:5001");
+});
